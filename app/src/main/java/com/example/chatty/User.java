198a65c762e4c40195e23d3cce72a7256a308553
Dredgegdix <dredgegdix@yandex.ru>
2023-04-04 13:30:07 +0400

    go right no left

--- app/src/main/java/com/example/chatty/User.java
+++ app/src/main/java/com/example/chatty/User.java
@@ -4,4 +4,8 @@ import java.io.Serializable;
 
 public class User implements Serializable {
     public String name, image, email, token, id;
+
+    public String getName(){
+        return name;
+    }
 }
--- app/src/main/java/com/example/chatty/UserActivity.java
+++ app/src/main/java/com/example/chatty/UserActivity.java
@@ -1,22 +1,27 @@
 package com.example.chatty;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.chatty.databinding.ActivityUserBinding;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class UserActivity extends AppCompatActivity implements UserListener{
 
+    private static final int MAX_LENGTH_SIZE = 15;
     private  ActivityUserBinding binding;
     private Preference preference;
+    private List<User> users = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +35,16 @@ public class UserActivity extends AppCompatActivity implements UserListener{
 
     private void setListeners(){
         binding.backImage.setOnClickListener(v -> onBackPressed());
+        binding.searchProfile.setOnClickListener(v -> {
+            if (ruleSearch(binding.search.getText().toString())){
+                searchUser();
+            }
+        });
+        binding.searchImg.setOnClickListener(v -> binding.linerLayoutSearch.setVisibility(View.VISIBLE));
+    }
+
+    private void showToast(String toast){
+        Toast.makeText(getApplicationContext(), toast , Toast.LENGTH_SHORT).show();
     }
 
     private void getUsers(){
@@ -41,7 +56,6 @@ public class UserActivity extends AppCompatActivity implements UserListener{
                     loading(false);
                     String currentUserId = preference.getString(Constants.USER_ID);
                     if(task.isSuccessful() && task.getResult() != null){
-                        List<User> users = new ArrayList<>();
                         for (QueryDocumentSnapshot queryDocumentSnapshot : task.getResult()) {
                             if (currentUserId.equals(queryDocumentSnapshot.getId())) {
                                 continue;
@@ -56,6 +70,7 @@ public class UserActivity extends AppCompatActivity implements UserListener{
                             }
                         }
                             if (users.size() > 0){
+                                Collections.shuffle(users);
                                 UserAdapter userAdapter = new UserAdapter(users, this);
                                 binding.usersRecyclerView.setAdapter(userAdapter);
                                 binding.usersRecyclerView.setVisibility(View.VISIBLE);
@@ -87,5 +102,35 @@ public class UserActivity extends AppCompatActivity implements UserListener{
         intent.putExtra(Constants.KEY_USER, user);
         startActivity(intent);
         finish();
+    }
+
+    private void searchUser(){
+        String search = binding.search.getText().toString();
+        Collections.sort(users, new Comparator<User>() {
+            @Override
+            public int compare(User user1, User user2) {
+                if (user1.getName().contains(search) && !user2.getName().contains(search)) {
+                    return -1;
+                } else if (!user1.getName().contains(search) && user2.getName().contains(search)) {
+                    return 1;
+                } else {
+                    return user1.getName().compareTo(user2.getName());
+                }
+            }
+        });
+        UserAdapter userAdapter = new UserAdapter(users, this);
+        binding.usersRecyclerView.setAdapter(userAdapter);
+    }
+
+    private Boolean ruleSearch(String search){
+        if (search.isEmpty()){
+            showToast("Введите запрос");
+            return false;
+        }if (search.length() > MAX_LENGTH_SIZE){
+            showToast("Слишком длинный запрос");
+            return false;
+        }else {
+            return true;
+        }
     }
 }
